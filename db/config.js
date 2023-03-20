8a03dc0185fbdf316459adeefae2961e369a40ff
Anupam Nagar <anupamnagar07@gmail.com>
2023-03-21 01:25:50 +0530

    atlas

--- db/config.js
+++ db/config.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/e-commerce');
+// mongoose.connect('mongodb://127.0.0.1:27017/e-commerce');
+mongoose.connect("mongodb+srv://aman:aman%40123@cluster0.oqgjq1r.mongodb.net/e-commerce?retryWrites=true&w=majority");
